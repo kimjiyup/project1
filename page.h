4c64a2cf6e7ef195e0de9722b2c3d2d35a8efb27
kjy4076 <kimjiyup4076@naver.com>
2023-05-05 18:13:24 +0900

    page.h fixed

--- page.h
+++ page.h
@@ -1,5 +1,13 @@
 class Page {
     public:
+        Page(int x,int y,int width,int height,int id,char content):x(x),y(y),width(width),height(height),id(id),content(content)
+        {}
+        int get_x() const { return x;}
+        int get_y() const { return y;}
+        int get_width() const { return width;}
+        int get_height() const { return height;}
+        int get_id() const { return id;}
+        char get_content() const { return content;}
 
     private:
         int x, y; // position of the page on the board
